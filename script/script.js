254fc48f0b3ecc5b39fc69ba231fdd0da90c0951
Nakamura Yukiko <okirakugama@yahoo.co.jp>
2021-06-30 20:21:56 +0900

    no message

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+  $('.burger-btn').on('click',function(){
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').toggleClass('slide-in');
+    $('body').toggleClass('noscroll');
+  });
+  
   
 });
+$(document).ready(function(){
+  $('.slick01').slick({
+    autoplay: false, 
+    dots: true,
+    arrows: true,
+  });
+})
